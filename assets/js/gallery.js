952a4eabccbc32b3db04cab089786dd2c7906a66
Katerina <yelkaterina@gmail.com>
2023-10-23 00:29:09 +0200

    Photo gallery (#53)
    * Update README file
    
    * Add basic structure
    
    * Set up photo gallery
    
    * Add experimental colors for background
    
    * Add files to assets folder, add css folder, update links
    
    * Add experimental fonts
    
    * Update gallery styles
    
    * Add style.css and some general styles
    
    * Add styling effects to the photos
    
    * Add images for home page
    
    * Update links to display home page images
    
    * Update home page
    
    * Put homepage styles in style.css, update index stylesheet link
    
    * Update home page styles
    
    * Update banner title
    
    * Add sound effect to game button on home page
    
    * Add DOM event listener for scripts.js
    
    * Fix banner
    
    * Update styles in home page
    
    * Add photos to gallery
    
    * Add gallery title and styles
    
    * Update home page content

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,18 +1,18 @@
 const photoFileName = [
+    ["halloween3", "halloween3-thumbnail"],
+    ["halloween7", "halloween7-thumbnail"],
     ["halloween-party", "halloween-party-thumbnail"],
+    ["halloween6", "halloween6-thumbnail"],
+    ["halloween5", "halloween5-thumbnail"],
+    ["halloween4", "halloween4-thumbnail"],
+    ["halloween2", "halloween2-thumbnail"],
     ["halloween-party1", "halloween-party1-thumbnail"],
-    ["halloween-party", "halloween-party-thumbnail"],
-    ["halloween-party1", "halloween-party1-thumbnail"],
-    ["halloween-party", "halloween-party-thumbnail"],
-    ["halloween-party1", "halloween-party1-thumbnail"],
-
 ]
 
 
 const imgFolder = "assets/images/img_gallery/";
 const imgFolderThumb = "assets/images//img_gallery/thumbnails/";
 let photo = "";
-// let photoThumb ="";
 
 photoFileName.map((fileName) => {
     const imgSrc = imgFolder + fileName[0];
